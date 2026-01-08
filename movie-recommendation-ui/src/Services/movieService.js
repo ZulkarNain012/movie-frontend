1c313fed7f455b747d1dbfd090fd788ab0e767ef
012ZulkarNain <012zulkarnain@gmail.com>
2026-01-08 10:05:06 +0530

    fix production api url

--- movie-recommendation-ui/src/Services/movieService.js
+++ movie-recommendation-ui/src/Services/movieService.js
@@ -2,25 +2,25 @@
 
 export const getMovieRecommendations = async (userInput) => {
   try {
-    const response = await fetch(
-      "https://movie-backend-kod1.onrender.com/api/recommend",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userInput }),
-      }
-    );
+    console.log("Fetching recommendations for input:", userInput);
+    const response = await fetch("http://localhost:5000/api/recommend", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }), 
+    });
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
 
     const data = await response.json();
-    return data.movies;
+    console.log("Received data:", data);
+    return data.movies; 
   } catch (error) {
     console.error("Error fetching movies:", error);
     return [];
   }
 };
+
